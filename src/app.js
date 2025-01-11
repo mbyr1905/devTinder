d7224fc1c016a56d40c3838245f614cfc98f115f
mbyr1905 <mb922043@student.nitw.ac.in>
2025-01-11 21:54:25 +0530

    Bug fix of order changing

--- src/app.js
+++ src/app.js
@@ -2,14 +2,13 @@ const express = require('express');
 
 const app = express()
 
-app.use('/', (req,res)=>{
-    res.send("hello from the server")
-})
-
 app.use('/test', (req,res)=>{
     res.send("testing")
 })
 
+app.use('/', (req,res)=>{
+    res.send("hello from the server")
+})
 
 app.listen(3000, () =>{
     console.log('server is successfully listening on port 3000')
